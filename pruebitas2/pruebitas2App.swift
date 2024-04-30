8e76de1d3e02c62a68be6957a3a2e55cea037e03
Isidro <peditobermudes@hotmail.com>
2024-04-30 12:26:14 -0600

    Initial Commit

---
+++ pruebitas2/pruebitas2App.swift
@@ -0,0 +1,17 @@
+//
+//  pruebitas2App.swift
+//  pruebitas2
+//
+//  Created by Isidro Jesus Muñoz on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct pruebitas2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
